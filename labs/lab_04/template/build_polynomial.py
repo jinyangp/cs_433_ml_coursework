ba27ad306fb293fbbf0dc51ea706d05fd1f71837
jinyangp <jinyangpeh@gmail.com>
2023-10-16 22:18:23 +0200

    lab 04 workings

---
+++ labs/lab_04/template/build_polynomial.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""implement a polynomial basis function."""
+
+import numpy as np
+
+
+def build_poly(x, degree):
+    """polynomial basis functions for input data x, for j=0 up to j=degree.
+
+    Args:
+        x: numpy array of shape (N,), N is the number of samples.
+        degree: integer.
+
+    Returns:
+        poly: numpy array of shape (N,d+1)
+
+    >>> build_poly(np.array([0.0, 1.5]), 2)
+    array([[1.  , 0.  , 0.  ],
+           [1.  , 1.5 , 2.25]])
+    """
+    # ***************************************************
+    # COPY YOUR CODE FROM EX03 HERE
+    # polynomial basis function: TODO
+    # this function should return the matrix formed
+    # by applying the polynomial basis to the input data
+    
+    res = np.empty((x.shape[0], degree + 1), dtype = np.float64)
+    for d in range(degree + 1):
+        res[:, d] = np.apply_along_axis(lambda s: s**d, 0, x)
+    
+    return res
+    # ***************************************************
---
+++ labs/lab_04/template/ridge_regression.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""Exercise 3.
+
+Ridge Regression
+"""
+
+import numpy as np
+
+
+def ridge_regression(y, tx, lambda_):
+    """implement ridge regression.
+
+    Args:
+        y: numpy array of shape (N,), N is the number of samples.
+        tx: numpy array of shape (N,D), D is the number of features.
+        lambda_: scalar.
+
+    Returns:
+        w: optimal weights, numpy array of shape(D,), D is the number of features.
+
+    >>> ridge_regression(np.array([0.1,0.2]), np.array([[2.3, 3.2], [1., 0.1]]), 0)
+    array([ 0.21212121, -0.12121212])
+    >>> ridge_regression(np.array([0.1,0.2]), np.array([[2.3, 3.2], [1., 0.1]]), 1)
+    array([0.03947092, 0.00319628])
+    """
+    # ***************************************************
+    # COPY YOUR CODE FROM EX03 HERE
+    # ridge regression: TODO
+    num_samples = y.shape[0]
+    num_params = tx.shape[1]
+    
+    inverse_product = np.linalg.inv(np.matmul(tx.T, tx) + (2*num_samples*lambda_)*np.identity(num_params))
+    optimal_w = np.matmul(inverse_product, tx.T).dot(y)
+    
+    return optimal_w
+    # ***************************************************
